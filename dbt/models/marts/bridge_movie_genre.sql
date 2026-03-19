8343570d38caea41a687dff3cc916f80eb99b2df
stiipp <curilansteph456@gmail.com>
2026-03-19 11:00:00 +0800

    dbt: restore bridge tables for reusable many-to-many dimensions

--- dbt/models/marts/bridge_movie_genre.sql
+++ dbt/models/marts/bridge_movie_genre.sql
@@ -1,4 +1,16 @@
-select
-  movie_id,
-  genre
-from {{ ref('stg_movie_genres') }}
+with src as (
+    select
+        movie_id,
+        trim(genre) as genre
+    from {{ ref('stg_movie_genres') }}
+    where genre is not null
+        and trim(genre) <> ''
+)
+
+select distinct
+    s.movie_id,
+    d.genre_key,
+    d.genre
+from src s
+join {{ ref('dim_genre') }} d
+    on s.genre = d.genre
